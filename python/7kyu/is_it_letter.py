6c4c4f82e476e1e423a2c29e8c346e555efa4dbe
Nadim Rai <nadim_rai@yahoo.com>
2024-03-25 03:49:41 +0800

    Is It A Letter

---
+++ python/7kyu/is_it_letter.py
@@ -0,0 +1,9 @@
+'''
+Write a function, isItLetter or is_it_letter, which tells us if a given character is an, 
+uppercase or lowercase, letter.
+'''
+
+def is_it_letter(s):
+     return s.isalpha()
+
+print(is_it_letter("1"))        #false
